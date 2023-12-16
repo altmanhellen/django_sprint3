3681ca018c86c00cd7140d49ddb4fa924d94b17d
Elena Altman <altmanhellen@gmail.com>
2023-12-16 17:12:58 +0200

    Delete empty files

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class PublishedModel(models.Model):
     """Абстрактная модель. Добавляет флаг is_published."""
+
     is_published = models.BooleanField(
         default=True,
         verbose_name='Опубликовано',
