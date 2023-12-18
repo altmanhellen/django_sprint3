514385012b5ae424f9342576a74956da8b31144d
Elena Altman <altmanhellen@gmail.com>
2023-12-18 19:08:00 +0200

    Change the the receipt of filtered querysets for post_list and category.

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,36 +1,24 @@
 from django.shortcuts import get_object_or_404, render
-from django.utils import timezone
 
-from .models import Category, Post
+from .models import Category
+from .querysets import FilteredQuerySet
+from .utils import get_post_list
 
 
 RECENT_POSTS_LIMIT = 5
 
 
-def published_recent_posts(category=None):
-    posts = Post.objects.filter(
-        is_published=True,
-        pub_date__lte=timezone.now(),
-        category__is_published=True
-    )
-    if category:
-        posts = posts.filter(category=category)
-    return posts
-
-
 def index(request):
-    post_list = published_recent_posts()
     context = {
-        'post_list': post_list[:RECENT_POSTS_LIMIT],
+        'post_list': get_post_list()[:RECENT_POSTS_LIMIT],
     }
     template_name = 'blog/index.html'
     return render(request, template_name, context)
 
 
 def post_detail(request, id):
-    post_list = published_recent_posts()
     post = get_object_or_404(
-        post_list,
+        get_post_list(),
         id=id
     )
     context = {
@@ -42,11 +30,10 @@ def post_detail(request, id):
 
 def category_posts(request, category_slug):
     category = get_object_or_404(
-        Category,
-        slug=category_slug,
-        is_published=True
+        FilteredQuerySet(Category).is_published(),
+        slug=category_slug
     )
-    post_list = published_recent_posts(category=category)
+    post_list = get_post_list().filter(category=category)
     context = {
         'category': category,
         'post_list': post_list
